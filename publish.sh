ae9293619f7719d96481e8a6f04cd3db6676c720
Shogo Kitada <shogo.kitada@outlook.jp>
2022-01-23 11:27:28 +0900

    publish.sh に html のエクスポートを確認する手順を追加

--- publish.sh
+++ publish.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-git add .
-git commit -m "$1"
-git push origin main
+read -n1 -p "Did you exported html? [y/N]: " yn
+if [[ $yn = [yY] ]]; then
+    git add .
+    git commit -m "$1"
+    git push origin main
+else
+    echo ""
+    echo "aborted"
+fi
